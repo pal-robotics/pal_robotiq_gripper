4996b9f0647c2a5c559f5a21a3a8f9187e01892a
thomaspeyrucain <thomas.peyrucain@pal-robotics.com>
2022-12-02 10:36:47 +0100

    Change is_grasped topic name for the epick gripper to be consistent

--- pal_robotiq_gripper_wrapper/scripts/gripper_epick_grasping.py
+++ pal_robotiq_gripper_wrapper/scripts/gripper_epick_grasping.py
@@ -38,7 +38,7 @@ class GripperGrasp(object):
         self.pub_gth = rospy.Publisher("{}/gripper_status_human".format(self.gripper_motor_name), String, queue_size=1)
 
         # Publish a boolean to know if an object is grasped or not
-        self.pub_js = rospy.Publisher("{}/is_grasped".format(self.gripper_motor_name), Bool , queue_size=1)
+        self.pub_js = rospy.Publisher("{}/is_grasped".format(self.controller_name), Bool , queue_size=1)
 
         # This node Dynamic params
         self.ddr = DDynamicReconfigure(
