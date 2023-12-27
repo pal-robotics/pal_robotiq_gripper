f0a702baf0f9d5991a6ff39d95da2bfc423731b3
Aina Irisarri <aina.irisarri@pal-robotics.com>
2023-12-27 12:49:50 +0100

    adapting launch files into the new launch files structure

---
+++ pal_robotiq_controller_configuration/launch/robotiq_gripper_controller.launch.py
@@ -0,0 +1,76 @@
+# Copyright (c) 2023 PAL Robotics S.L. All rights reserved.
+#
+# Unauthorized copying of this file, via any medium is strictly prohibited,
+# unless it was supplied under the terms of a license agreement or
+# nondisclosure agreement with PAL Robotics SL. In this case it may not be
+# copied or disclosed except in accordance with the terms of that agreement.
+
+import os
+
+from dataclasses import dataclass
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import PythonExpression
+
+from ament_index_python.packages import get_package_share_directory
+from controller_manager.launch_utils import generate_load_controller_launch_description
+from launch_pal.param_utils import parse_parametric_yaml
+from launch_pal.arg_utils import LaunchArgumentsBase, read_launch_argument
+from launch.actions import DeclareLaunchArgument, SetLaunchConfiguration, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+from launch import LaunchDescription, LaunchContext
+
+
+@dataclass(frozen=True)
+class LaunchArguments(LaunchArgumentsBase):
+
+    side: DeclareLaunchArgument = DeclareLaunchArgument(
+        name='side',
+        default_value='',
+        choices=['', 'left', 'right'],
+        description='side of the end effector')
+    # model: DeclareLaunchArgument = DeclareLaunchArgument(
+    #     name='model', # Current models are robotiq-2f-85 and robotiq-2f-140 
+    #     choices=['robotiq-2f-85', 'robotiq-2f-140'],
+    #     description='model of the robotiq gripper')
+
+def declare_actions(launch_description: LaunchDescription, launch_args: LaunchArguments):
+    launch_description.add_action(OpaqueFunction(function=setup_controller_configuration))
+    launch_controller = generate_load_controller_launch_description(
+        controller_name=LaunchConfiguration("controller_name"),
+        controller_type='position_controllers/JointTrajectoryController',
+        controller_params_file=LaunchConfiguration("controller_config"))
+
+    launch_description.add_action(launch_controller)
+
+    return
+
+def setup_controller_configuration(context: LaunchContext):
+
+    side = read_launch_argument('side', context)
+    ee_prefix = "gripper"
+    if side:
+        ee_prefix = f"gripper_{side}"
+
+    controller_name = f"{ee_prefix}_controller"
+    remappings = {"EE_SIDE_PREFIX": ee_prefix}
+    param_file = os.path.join(
+        get_package_share_directory('pal_robotiq_controller_configuration'),
+        'config', 'robotiq_gripper_controller.yaml')
+
+    parsed_yaml = parse_parametric_yaml(source_files=[param_file], param_rewrites=remappings)
+
+    return [SetLaunchConfiguration('controller_name', controller_name),
+            SetLaunchConfiguration('controller_config', parsed_yaml)]
+
+def generate_launch_description():
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+    launch_arguments = LaunchArguments()
+
+    launch_arguments.add_to_launch_description(ld)
+
+    declare_actions(ld, launch_arguments)
+
+    return ld
